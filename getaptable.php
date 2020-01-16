2d10099f4b7739670df142a319553f3b33fec4db
mikhail.belyankin <mikhail.belyankin@exactprosystems.com>
2020-01-16 16:21:29 +0300

    new tables with set ap status

--- getaptable.php
+++ getaptable.php
@@ -1,6 +1,84 @@
 <?php
   require_once 'vendor/autoload.php';
   include('config.php');
+
+
+echo '<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+function sayHi() {
+  $("#div1").html("ready to serve");
+}
+    $(document).ready(function(){
+	$(\'.actionopen\').click(function(){
+	    var id = $(this).attr(\'id\');
+	    ajaxOpen(id);
+	})
+
+	$(\'.actionlock\').click(function(){
+	    var id = $(this).attr(\'id\');
+	    ajaxLock(id);
+	})
+	$(\'.actionunlock\').click(function(){
+	    var id = $(this).attr(\'id\');
+	    ajaxUnlock(id);
+	})
+	$(\'.actionreset\').click(function(){
+	    var id = $(this).attr(\'id\');
+	    ajaxReset(id);
+	})
+
+	function ajaxOpen(param){
+	    $.get(
+		\'openap.php\',
+        	{\'id\':param},
+		function(data){
+		    $("#div1").html(data);setTimeout(sayHi, 2000);
+		});
+	    }
+
+	function ajaxLock(param){
+	    $.get(
+		\'setap.php\',
+        	{\'id\':param,\'state\':"LOCKED"},
+		function(data){
+		    $("#div1").html(data);setTimeout(sayHi, 2000);
+		});
+	    }
+
+	function ajaxUnlock(param){
+	    $.get(
+		\'setap.php\',
+        	{\'id\':param,\'state\':"UNLOCKED"},
+		function(data){
+		    $("#div1").html(data);setTimeout(sayHi, 2000);
+		});
+	    }
+
+	function ajaxReset(param){
+	    $.get(
+		\'setap.php\',
+        	{\'id\':param},
+		function(data){
+		    $("#div1").html(data);setTimeout(sayHi, 2000);
+		});
+	    }
+
+
+function sayHi() {
+	  $("#div1").html("ready to serve");
+	}
+    });
+</script>
+</head>
+<body>
+
+<div id="div1"><h2>Let SKUD open the doors</h2></div>';
+
+
+
   $client = Graze\TelnetClient\TelnetClient::factory();
   $promptError = 'ERROR (?<errorNum>[0-9]) (?<errorDesc>.*)';
   $lineEnding = "\r\n";
@@ -8,7 +86,6 @@
 #  $id = isset($_GET['id']) ?" OBJECTID " . $_GET['id'] : "ALL";
   echo "Login: ";
   print_r($client->execute($auth_string)->getResponseText());
-
 
   $cmd= "GETZONEINFO";
   $result=$client->execute($cmd)->getResponseText();
@@ -23,7 +100,7 @@
 //  print_r($zones);
 
 
-  $output = "<TABLE border=1><TR><TH>ID</TH><TH>ACCESS POINT</TH><TH>FROM</TH><TH>TO</TH><TH>STATUS</TH><TH>STATE</TH></TR>";
+  $output = "<TABLE border=1><TR><TH>ID</TH><TH>ACCESS POINT</TH><TH>FROM</TH><TH>TO</TH><TH>STATUS</TH><TH>STATE</TH><TH>ACTION</TH></TR>";
   $cmd= "GETAPLIST";
 //  echo "<br/> Command: $cmd <br/>";
   $result=$client->execute($cmd)->getResponseText();
@@ -40,16 +117,21 @@
 //    print_r($apinfo);
     $output .= "<TR>";
     $length = count($apinfo);
+    $apid=-1;
     for ($i = 0; $i < $length; $i++) {
         switch ($apinfo[$i])
           {
-            case "ID": $output .= "<TD>".$apinfo[$i+1]."</TD>"; break;
+            case "ID": $output .= "<TD>".$apinfo[$i+1]."</TD>"; $apid=$apinfo[$i+1]; break;
             case "NAME": $output .= "<TD>".$apinfo[$i+1]."</TD>"; break;
             case "ZONEA": $output .= "<TD>".$zones[$apinfo[$i+1]]."</TD>"; break;
             case "ZONEB": $output .= "<TD>".$zones[$apinfo[$i+1]]."</TD>"; break;
             case "STATE": $output .= "<TD>".$apinfo[$i+1]."</TD><TD>".$apinfo[$i+2]."</TD>"; break;
           }
     }
+#    $output .= "<TD><button id='b".$apid.
+#"' onclick='{ $.ajax({url: \"openap.php?id=".$apid.
+#"\", success: function(result){ $(\"#div1\").html(result);setTimeout(sayHi, 2000);}});}'>Open</button></TD>";
+    $output .= "<TD><button id='".$apid."' class='actionopen'>Open</button> <button id='".$apid."' class='actionlock'>Lock</button><button id='".$apid."' class='actionreset'>NORMAL</button><button id='".$apid."' class='actionunlock'>Unlock</button></TD>";
     $output .= "</TR>";
    }
     $output .= "</TABLE>";
---
+++ setap.php
@@ -0,0 +1,17 @@
+<?php
+
+  require_once 'vendor/autoload.php';
+  include('config.php');
+  $client = Graze\TelnetClient\TelnetClient::factory();
+  $promptError = 'ERROR (?<errorNum>[0-9]) (?<errorDesc>.*)';
+  $lineEnding = "\r\n";
+  $client->connect($dsn, "", $promptError,$lineEnding);
+  $id = isset($_GET['id']) ?$_GET['id'] : "-1";
+  if ($_GET["state"]) $state=$_GET["state"]; else $state="NORMAL";
+
+  $cmd= "SETAPMODE $state $id";
+  echo "Login: ";
+  print_r($client->execute($auth_string)->getResponseText());
+  echo "<br/> Command: $cmd <br/>";
+  print_r($client->execute($cmd)->getResponseText());
+?>
